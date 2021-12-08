4c5c29f5fe2b5dbb0c44dfc22229f4feff6fb9f7
bethanyuo <bethanyosueke@gmail.com>
2021-12-08 08:19:35 -0600

    updated Token with bool constraints

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NEWToken is ERC20 {
-    uint256 _startTime;
-    uint256 _endTime;
+    uint256 public _startTime;
+    uint256 public _endTime;
+    bool public hasStarted;
+    bool public hasEnded;
     address owner;
 
     event Transfers(address recipient, uint tokensSent, uint currentTime);
@@ -15,11 +17,14 @@ contract NEWToken is ERC20 {
         _mint(owner, 1000);
         _startTime = block.timestamp;
         _endTime = _startTime + 3 minutes;
+        hasStarted;
+        !hasEnded;
     }
 
     modifier timeCheck() {
         require(_endTime >= block.timestamp, "Contribution Time is Over");
         _;
+        hasEnded = true;
     }
 
     function transferr(address recipient, uint256 amount) public timeCheck {
--- test/token-test.js
+++ test/token-test.js
@@ -1,20 +1,49 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require( "chai" );
+const { ethers } = require( "hardhat" );
+const { time } = require( "@openzeppelin/test-helpers" )
 
-describe("NEWToken", function () {
-  it("Should transfer tokens during set timeframe", async function () {
-    const Token = await ethers.getContractFactory("NEWToken");
-    const token = await Token.deploy();
-    await token.deployed();
+describe( "NEWToken", function () {
+    let startTime, endTime;
 
-    expect(await token._startTime).to.equal(block.timestamp);
-    // expect(await token.transferr()).to.equal("Hello, world!");
 
-    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    // beforeEach(async () => {
+    //     startTime = (await time.latest());
+    //     endTime = (await time.latest()).add(time.duration.minutes(3));
+    // })
+   
+    it( "Should transfer at starting time", async () => {
+        const Token = await ethers.getContractFactory( "NEWToken" );
+        const token = await Token.deploy();
+        await token.deployed();
+        //startTime = (await time.latest());
+        // endTime = (await time.latest()).add(time.duration.minutes(3));
 
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
+        expect( await token._startTime() ).to.equal( null );
+        // expect(await token.transferr()).to.equal("Hello, world!");
 
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+        // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+        // // wait until the transaction is mined
+        // await setGreetingTx.wait();
+
+        // expect(await greeter.greet()).to.equal("Hola, mundo!");
+    } );
+
+    it( "Should stop transfer at ending time", async () => {
+        const Token = await ethers.getContractFactory( "NEWToken" );
+        const token = await Token.deploy();
+        await token.deployed();
+        // startTime = (await time.latest());
+        // endTime = (await time.latest()).add(time.duration.minutes(3));
+
+        // expect( await token._startTime ).to.equal( startTime );
+        // expect(await token.transferr()).to.equal("Hello, world!");
+
+        // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+        // // wait until the transaction is mined
+        // await setGreetingTx.wait();
+
+        // expect(await greeter.greet()).to.equal("Hola, mundo!");
+    } );
+} );
