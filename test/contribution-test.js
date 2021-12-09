ff208462b6d9a06fef9b295af1eaac65c8810e3f
bethanyuo <bethanyosueke@gmail.com>
2021-12-08 20:22:20 -0600

    created Auction event test

--- test/contribution-test.js
+++ test/contribution-test.js
@@ -6,7 +6,7 @@ const { MockProvider } = require( 'ethereum-waffle' );
 describe( "Contribution", function () {
     let contract;
 
-    const [walletTo] = new MockProvider().getWallets();
+    const [wallet, walletTo] = new MockProvider().getWallets();
 
     const overrides = {
         gasLimit: 1000000000000000,
@@ -26,6 +26,12 @@ describe( "Contribution", function () {
     it( "Should get all ETH sent by address", async () => {
         //expect( await contract.getBalance(walletTo.address) ).to.gt(0);
         expect( await contract.getBalance( walletTo.address ) ).to.equal( overrides.value );
+    } );
+
+    it( "Should  emit successful Auction event", async () => {
+        await expect( contract.exchange( walletTo.address, 10) )
+            .to.emit( contract, 'Auction' )
+            .withArgs( wallet.address, overrides.value, 10 );
     } );
 
 } );
