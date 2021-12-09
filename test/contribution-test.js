d0f5b43bbab53de0bf115662f73eae39420fab6b
bethanyuo <bethanyosueke@gmail.com>
2021-12-08 18:06:26 -0600

    updated contribution test

--- test/contribution-test.js
+++ test/contribution-test.js
@@ -4,26 +4,28 @@ const { MockProvider } = require( 'ethereum-waffle' );
 
 
 describe( "Contribution", function () {
-    const [walletTo] = new MockProvider().getWallets();
     let contract;
+
+    const [walletTo] = new MockProvider().getWallets();
+
     const overrides = {
         gasLimit: 1000000000000000,
-        value: (ethers.utils.parseEther("0")).toNumber(),
+        value: ( ethers.utils.parseEther( "0" ) ).toNumber(),
     }
 
-    beforeEach(async () => {
+    beforeEach( async () => {
         let Contract = await ethers.getContractFactory( "Contribution" );
         contract = await Contract.deploy();
         await contract.deployed();
-    })
+    } )
 
     it( "Should exchange ETH for tokens else revert", async () => {
-        await expect(contract.exchange(walletTo.address, 10)).to.be.reverted;
+        await expect( contract.exchange( walletTo.address, 10 ) ).to.be.reverted;
     } );
 
     it( "Should get all ETH sent by address", async () => {
         //expect( await contract.getBalance(walletTo.address) ).to.gt(0);
-        expect( await contract.getBalance(walletTo.address) ).to.equal(overrides.value);
+        expect( await contract.getBalance( walletTo.address ) ).to.equal( overrides.value );
     } );
 
 } );
